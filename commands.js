989c7402ab1b0ca80985549ff9c1b6367fa37fb7
Ary Yakos <y3x0@hotmail.com>
2023-12-24 19:16:58 +0000

    Added and started command dictionary.
    Co-authored-by: Ramone Graham <ramonegraham@gmail.com>

--- commands.js
+++ commands.js
@@ -1,13 +1,96 @@
-help = [
-    'Normal portfolio commands',
-    'whois      -  Who are you? And what are you doing in my website >:(',
-    'intro      -  Who am i?',
-    'about      -  What do I do?',
-    'tpdc       -  what is tpdc?',
-    'projects   -  My projects that i have done so far',
-    '-------------------------------------------------------------',
-    'Basic Linux Commands',
-    'ls',
-    'cd',
-    'whoami'
-]
+// export const help = [
+//     'Normal portfolio commands',
+//     'whois      -  Who are you? And what are you doing in my website >:(',
+//     'intro      -  Who am i?',
+//     'about      -  What do I do?',
+//     'tpdc       -  what is tpdc?',
+//     'projects   -  My projects that i have done so far',
+//     'socials    -  My socials tf you thankin?',
+//     '-------------------------------------------------------------',
+//     'Basic Linux Commands',
+//     'ls',
+//     'cd',
+//     'whoami'
+// ];
+
+const help = {
+    name: "help",
+    description: "Shows all available commands",
+    execute() {
+
+    }
+};  
+
+const whois = {
+    name: "whois",
+    description: "Who are you? And what are you doing in my website >:(",
+    execute() {
+
+    }
+};
+
+const intro = {
+    name: "intro",
+    description: "Hi",
+    execute() {
+
+    }
+};
+
+const about = {
+    name: "about",
+    description: "Who am I, and what do I do?",
+    execute() {
+
+    }
+};
+
+const tpdc = {
+    name: "tpdc",
+    description: "What is TPDC?",
+    execute() {
+
+    }
+};
+
+const projects = {
+    name: "projects",
+    description: "My projects that i have worked on and am going to work on!",
+    execute() {
+
+    }
+};
+
+const socials = {
+    name: "socials",
+    description: "My socials",
+    execute() {
+
+    }
+};
+
+const ls = {
+    name: "ls",
+    description: "Lists everything in the server",
+    execute() {
+
+    }
+};
+
+const cd = {
+    name: "cd",
+    description: "Changes directory",
+    execute() {
+
+    }
+};
+
+const whoami = {
+    name: "whoami",
+    description: "Shows your rank in the program",
+    execute() {
+
+    }
+};
+
+// Module exports
